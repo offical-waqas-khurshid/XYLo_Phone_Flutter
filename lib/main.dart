5a9518fe036e1a6191a9b6e528c90c4692ae00c0
Waqas Khurshid <waqaskhurshidoffical@gmail.com>
2023-04-07 23:41:43 +0500

    xylo phone

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              getView(1, Colors.red),
+              getView(2, Colors.yellow),
+              getView(3, Colors.green),
+              getView(4, Colors.lime),
+              getView(5, Colors.brown),
+              getView(6, Colors.blueAccent),
+              getView(7, Colors.purple),
+            ],
+          ),
+        )),
+  ));
+}
+
+Widget getView(int pos, Color color) {
+  return Expanded(
+    child: TextButton(
+      style: ButtonStyle(backgroundColor: MaterialStateProperty.all(color)),
+      onPressed: () {
+        playSound(pos);
+      },
+      child: const Text(
+        "Click Me",
+        style: TextStyle(
+            fontWeight: FontWeight.w700, color: Colors.white, fontSize: 22),
+      ),
+    ),
+  );
+}
+
+void playSound(int pos) {
+  final audioPlayer = AudioCache();
+  audioPlayer.play('assets_note$pos.wav');
+}
